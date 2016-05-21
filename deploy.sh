01df1b33422ea7101a3bdba888656fafb09a4187
Taisuke Miyazaki <imishinist@gmail.com>
2016-05-21 23:00:28 +0900

    オプション変更

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-ssh $USER@$DEPLOY_HOST 'cd ayb_bot; eval `ssh-agent`; ssh-add ~/.ssh/travis_key; git pull origin master'
+ssh -oStrictHostKeyChecking=no $USER@$DEPLOY_HOST 'cd ayb_bot; eval `ssh-agent`; ssh-add ~/.ssh/travis_key; git pull origin master'
